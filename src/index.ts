0310636df6586875ab959297bb0cbb94083b1d2e
Vadim Budarin <budarin-vv@yandex.ru>
2023-09-20 17:09:07 +0300

    rename node version

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
+import { taskQueue as nodeTaskQueue } from './nodeTaskQueue.js';
 import { taskQueue as schedulerQueue } from './schedulerQueue.js';
 import { taskQueue as postMessageTaskQueue } from './postMessageTaskQueue.js';
-import { taskQueue as setImmediateTaskQueue } from './setImmediateTaskQueue.js';
 
 const hasScheduler = 'scheduler' in globalThis;
 
-export const taskQueue = window ? (hasScheduler ? schedulerQueue : postMessageTaskQueue) : setImmediateTaskQueue;
+export const taskQueue = window ? (hasScheduler ? schedulerQueue : postMessageTaskQueue) : nodeTaskQueue;
