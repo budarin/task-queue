25bcb8e14b12029b26700e9ead233549c3b44bba
Vadim Budarin <budarin-vv@yandex.ru>
2023-09-20 18:59:50 +0300

    fix building

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,5 @@ set -e
 export "NODE_ENV"="production";
 
 rm -rf dist;
-tsc -p tsconfig.cjs.json;
 tsc -p tsconfig.esm.json;
 tsc -p tsconfig.types.json;
-echo '{"type": "commonjs"}' > dist/cjs/package.json
-echo '{"type": "module"}' > dist/esm/package.json
