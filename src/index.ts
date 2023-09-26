3b101402f5c5fc33fec35d8a84d5e8afd2d3b2ad
Vadim Budarin <budarin-vv@yandex.ru>
2023-09-26 17:01:26 +0300

    fix typos

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ const hasMessageChannel = 'MessageChannel' in globalThis;
 
 function getTaskQueue() {
     if (hasScheduler) {
-        return require('schedulerQueue.js').taskQueue;
+        return require('./schedulerQueue.js').taskQueue;
     }
 
     if (hasMessageChannel) {
-        return require('postMessageTaskQueue.js').taskQueue;
+        return require('./postMessageTaskQueue.js').taskQueue;
     }
 
-    return require('nodeTaskQueue.js').taskQueue;
+    return require('./nodeTaskQueue.js').taskQueue;
 }
 
 export const taskQueue = { taskQueue: getTaskQueue() };
