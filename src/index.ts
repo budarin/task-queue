70b60a59017c00381bd5b0ddbb451c229a86229c
Vadim Budarin <budarin-vv@yandex.ru>
2023-09-26 17:30:10 +0300

    fix taskQueue creation logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 const hasScheduler = 'scheduler' in globalThis;
 const hasMessageChannel = 'MessageChannel' in globalThis;
+const isNode = typeof process === 'object' && process + '' === '[object process]';
 
 function getTaskQueue() {
+    if (isNode) {
+        return require('./nodeTaskQueue.js');
+    }
+
     if (hasScheduler) {
         return require('./schedulerQueue.js');
     }
