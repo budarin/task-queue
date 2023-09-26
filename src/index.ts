be7cfe0d1e0c1d2aed9ca0c4fec9d6e113453927
Vadim Budarin <budarin-vv@yandex.ru>
2023-09-26 17:06:05 +0300

    fix bug

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ const hasMessageChannel = 'MessageChannel' in globalThis;
 
 function getTaskQueue() {
     if (hasScheduler) {
-        return require('./schedulerQueue.js').taskQueue;
+        return require('./schedulerQueue.js');
     }
 
     if (hasMessageChannel) {
-        return require('./postMessageTaskQueue.js').taskQueue;
+        return require('./postMessageTaskQueue.js');
     }
 
-    return require('./nodeTaskQueue.js').taskQueue;
+    return require('./nodeTaskQueue.js');
 }
 
-export const taskQueue = { taskQueue: getTaskQueue() };
+export const taskQueue = getTaskQueue();
